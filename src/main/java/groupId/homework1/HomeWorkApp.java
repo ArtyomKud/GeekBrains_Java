38c7646543213a810b8f1b937c1f85af33f6f50a
ArtyomKud <artyom_kudryavcev_91@mail.ru>
2022-04-17 17:44:41 +0700

    task3

--- src/main/java/groupId/homework1/HomeWorkApp.java
+++ src/main/java/groupId/homework1/HomeWorkApp.java
@@ -6,11 +6,28 @@ public class HomeWorkApp {
         System.out.println("Banana");
         System.out.println("Apple");
     }
+    public static void checkSumSign() {
+        int a = 10;
+        int b = 15;
+        int c = a+b;
+        if (c >=0) {
+            System.out.println("Сумма положительна");
+        }
+        else {
+            System.out.println("Сумма отрицательна");
+        }
+    }
+
 
         public static void main(String[] args) {
             printThreeWords();
+            checkSumSign();
 
-        }
+
+
+            }
+
+
 
 
 }
