8ebc195e21e76d8523ee788dd7b7fe9725a4a013
ArtyomKud <artyom_kudryavcev_91@mail.ru>
2022-04-17 17:13:13 +0700

    task2

--- src/main/java/groupId/homework1/HomeWorkApp.java
+++ src/main/java/groupId/homework1/HomeWorkApp.java
@@ -1,7 +1,16 @@
 package groupId.homework1;
 
 public class HomeWorkApp {
-    public static void main(String[] args) {
-
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
     }
+
+        public static void main(String[] args) {
+            printThreeWords();
+
+        }
+
+
 }
