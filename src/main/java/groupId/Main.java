fcf1f68a3d398600540622fa7bd97105ba9dae2b
ArtyomKud <artyom_kudryavcev_91@mail.ru>
2022-04-20 21:07:58 +0700

    task1

--- src/main/java/groupId/Main.java
+++ src/main/java/groupId/Main.java
@@ -2,7 +2,13 @@ package groupId;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Привет, мир!!");
+        int a = 15;
+        int b = 6;
+        boolean c = myMetod(a,b);
+        System.out.println(c);
 
+    }
+    public static boolean myMetod(int a, int b){
+        return a+b>=10&&a+b<=20;
     }
 }
