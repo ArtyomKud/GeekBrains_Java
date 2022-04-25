4901ed02fe02ef036ed0971d99004c3b28dc1f39
ArtyomKud <artyom_kudryavcev_91@mail.ru>
2022-04-25 23:12:33 +0700

    task1

---
+++ src/main/java/Home_work_3/MyClass.java
@@ -0,0 +1,21 @@
+package Home_work_3;
+
+import java.util.Arrays;
+
+public class MyClass {
+    public static void main(String[] args) {
+        int[] a = {1, 1, 0, 0, 1, 0, 1, 5, 0, 0};
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == 0) {
+                a[i] = 1;
+            }
+            else if (a[i] == 1) {
+                a[i] = 0;
+            }
+            else {
+
+            }
+        }
+            System.out.println(Arrays.toString(a));
+    }
+}
