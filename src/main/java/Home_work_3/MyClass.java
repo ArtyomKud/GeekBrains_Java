b76593aada0b79a9d8b62aa8e3fd46a571c972c6
ArtyomKud <artyom_kudryavcev_91@mail.ru>
2022-04-26 02:54:57 +0700

    task6

--- src/main/java/Home_work_3/MyClass.java
+++ src/main/java/Home_work_3/MyClass.java
@@ -25,44 +25,65 @@ public class MyClass {
         for (int i = 0; i < c.length; i++) {
             if (c[i] < 6) {
                 c[i] *= 2;
-            } else  {
+            } else {
             }
 
         }
         System.out.println(Arrays.toString(c));
-        int [][] d = new int[4][4];
-        for(int i=0,j=0,k=3; i< d.length&&j<d[i].length; i++, j++,k--){
-            if(i==j||i+k== d.length){
-                d[i][j]=1;
-                d[i][k]=1;
-            }
-            else{
+        int[][] d = new int[4][4];
+        for (int i = 0, j = 0, k = 3; i < d.length && j < d[i].length; i++, j++, k--) {
+            if (i == j || i + k == d.length) {
+                d[i][j] = 1;
+                d[i][k] = 1;
+            } else {
             }
         }
         myMetodPrint(d);
         int len = 4;
         int initialValue = 6;
-        System.out.println(Arrays.toString(myMetod(len,initialValue)));
+        System.out.println(Arrays.toString(myMetod(len, initialValue)));
+        int[] l = {5, 6,11, 9, 1, 0, -3};
+        myMetod2(l);
 
 
+    }
 
-
-        }
-        public static void myMetodPrint(int[][] d){
-        for(int i=0; i<d.length; i++){
-            for(int j = 0; j<d[i].length; j++){
+    public static void myMetodPrint(int[][] d) {
+        for (int i = 0; i < d.length; i++) {
+            for (int j = 0; j < d[i].length; j++) {
                 System.out.print(d[i][j]);
             }
             System.out.println();
         }
     }
-    public static int[] myMetod(int a, int b){
-        int x[] = new int [a];
-        for(int i = 0;i<x.length; i++){
-            x[i]=b;
+
+    public static int[] myMetod(int a, int b) {
+        int x[] = new int[a];
+        for (int i = 0; i < x.length; i++) {
+            x[i] = b;
 
         }
         return x;
 
+    }
+
+    public static void myMetod2(int[] a) {
+        int n = a[0];
+        int t = a[0];
+        if (a.length == 1) {
+            System.out.println("Массив содержит только одно значение:" + a[0]);
+        }
+        for (int i = 1; i < a.length; i++) {
+            if (n < a[i]) {
+                n = a[i];
+            }
+            if (t > a[i]) {
+                t = a[i];
+            }
+
+
+        }
+        System.out.println("Максимальное число массива:" + n);
+        System.out.println("Минимальное число массива:" + t);
     }
 }
