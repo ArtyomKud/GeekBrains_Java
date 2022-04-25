77ee993fe79b04bba6a667a46700ebd7ac6d296c
ArtyomKud <artyom_kudryavcev_91@mail.ru>
2022-04-25 23:28:55 +0700

    task3

--- src/main/java/Home_work_3/MyClass.java
+++ src/main/java/Home_work_3/MyClass.java
@@ -8,21 +8,27 @@ public class MyClass {
         for (int i = 0; i < a.length; i++) {
             if (a[i] == 0) {
                 a[i] = 1;
-            }
-            else if (a[i] == 1) {
+            } else if (a[i] == 1) {
                 a[i] = 0;
-            }
-            else {
+            } else {
 
             }
         }
-            System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(a));
 
         int[] b = new int[100];
-        for(int i =0; i<b.length;i++){
-            b[i]+=i+1;
+        for (int i = 0; i < b.length; i++) {
+            b[i] += i + 1;
         }
         System.out.println(Arrays.toString(b));
+        int[] c = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < c.length; i++) {
+            if (c[i] < 6) {
+                c[i] *= 2;
+            } else  {
+            }
 
+        }
+        System.out.println(Arrays.toString(c));
     }
 }
