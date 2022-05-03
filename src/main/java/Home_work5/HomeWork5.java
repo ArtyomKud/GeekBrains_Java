fed01960338400d2346baa6e3706a7cf44d303a9
ArtyomKud <artyom_kudryavcev_91@mail.ru>
2022-05-03 22:38:36 +0700

    task1

---
+++ src/main/java/Home_work5/HomeWork5.java
@@ -0,0 +1,8 @@
+package Home_work5;
+
+public class HomeWork5 {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/java/Home_work5/Person.java
@@ -0,0 +1,11 @@
+package Home_work5;
+
+public class Person {
+    private String fullName;
+    private String position;
+    private String email;
+    private long phoneNumber;
+    private long wage;
+    private int age;
+
+}
