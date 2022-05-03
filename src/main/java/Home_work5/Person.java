c5cb58f6661ce6bad8e2b2133a7151af0982e828
ArtyomKud <artyom_kudryavcev_91@mail.ru>
2022-05-03 22:50:44 +0700

    task2

--- src/main/java/Home_work5/Person.java
+++ src/main/java/Home_work5/Person.java
@@ -8,4 +8,22 @@ public class Person {
     private long wage;
     private int age;
 
+    public Person(String fullName, String position, String email, long phoneNumber, long wage, int age){
+        this.fullName=fullName;
+        this.position=position;
+        this.email=email;
+        this.phoneNumber=phoneNumber;
+        this.wage=wage;
+        setAge(age);
+
+    }
+
+    public void setAge(int age) {
+        if (age<0&&age>150){
+            this.age = age;
+        }
+        else {
+            System.out.println("Введен некорректный возраст!!!");
+        }
+    }
 }
