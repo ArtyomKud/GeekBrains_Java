e5dc324620ca4f9073c67a2d1423a47245ea7eb6
ArtyomKud <artyom_kudryavcev_91@mail.ru>
2022-06-13 21:19:09 +0700

    task1

---
+++ src/main/java/groupId/Task_test.java
@@ -0,0 +1,27 @@
+package groupId;
+
+import java.util.*;
+
+public class Task_test {
+    public static void main(String[] args) {
+
+        ArrayList<String> list1=new ArrayList(Arrays.asList("Банка", "Котел", "Автомобиль", "Банка", "Светофор", "Кружка", "Глобус", "Игрушка", "Автомобиль", "Розетка", "Светильник", "Подставка", "Подкова", "Тройник", "Телефон", "Светильник", "Банка", "Розетка", "Полка", "Шкаф"));
+
+        for (String o: list1) {
+            System.out.println(o);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
