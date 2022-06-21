b20d88f6e376ea86eaee50f1c678c42add5e66a3
ArtyomKud <artyom_kudryavcev_91@mail.ru>
2022-06-22 00:50:29 +0700

    task1

--- src/main/java/groupId/Main.java
+++ src/main/java/groupId/Main.java
@@ -1,8 +1,13 @@
 package groupId;
 
+import groupId.klienr_server.Klient;
+import groupId.klienr_server.Server;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Привет, мир!!");
+
+
+
 
     }
 }
